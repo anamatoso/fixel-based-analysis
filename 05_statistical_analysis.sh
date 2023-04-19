ba7a2295d3e91123b63b9083b4a105423e45e7b0
Ana Matoso <anamatoso92@gmail.com>
2023-04-19 09:39:56 +0100

    clean

--- 05_statistical_analysis.sh
+++ 05_statistical_analysis.sh
@@ -7,5 +7,3 @@ set -e
 fixelcfestats fd_smooth/ files.txt design_matrix.txt contrast_matrix.txt matrix/ stats_fd/
 fixelcfestats log_fc_smooth/ files.txt design_matrix.txt contrast_matrix.txt matrix/ stats_log_fc/
 fixelcfestats fdc_smooth/ files.txt design_matrix.txt contrast_matrix.txt matrix/ stats_fdc/
-
-
