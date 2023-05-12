c683bb28dc58052710d39c2204a0c17ac860a4bc
Ana Matoso <anamatoso92@gmail.com>
2023-05-12 12:01:01 +0100

    Refactor

--- 05_statistical_analysis.sh
+++ 05_statistical_analysis.sh
@@ -9,9 +9,9 @@ printf "0 -1 1" >> text_files/contrast_matrix.txt
 
 # Perform statistical analysis of FD, FC, and FDC
 metrics="fd log_fc fdc"
+comparisons="midinter midpre preict interict"
 
 for i in $metrics; do
-    fixelcfestats template/${i}_smooth/ files_${i}.txt design_matrix.txt contrast_matrix.txt template/matrix/ template/stats_${i}/ -force
     for c in $comparisons; do
         fixelcfestats template/${i}_smooth/ text_files/files_${i}_${c}.txt text_files/design_matrix_${c}.txt text_files/contrast_matrix.txt template/matrix/ template/stats_${i}_${c}/ -force
     done
