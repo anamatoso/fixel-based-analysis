40c1ab1118480337387935519a3fbb0980c0ecf9
Ana Matoso <anamatoso92@gmail.com>
2023-05-12 11:53:53 +0100

    Refactor and clean

--- 06_display_results.sh
+++ 06_display_results.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
-read -p 'Choose metric [fd|log_fc|fdc]: ' metric
+# Set variables for vizualization
+read -p 'Choose metric [fd | log_fc | fdc]: ' metric
+read -p 'Choose statistic [fwe_1mpvalue | uncorrected_pvalue | tvalue | Zstat]: ' stat
+read -p 'Choose comparison [midinter| midpre | preict | interpre]: ' comp
+read -p 'Choose contrast [sup | inf]: ' contrast
 
-# Map fixel values to streamline points
-fixel2tsf "template/stats_${metric}/fwe_1mpvalue_t1.mif" template/tracks_100k_sift.tck "template/${metric}_fwe_1mpvalue.tsf" -force
-#fixel2tsf "template/stats_${metric}/abs_effect_size.mif" tracks_100k_sift.tck "${metric}_abs_effect_size.tsf"
-#fixel2tsf "template/stats_${metric}/tvalue.mif" tracks_100k_sift.tck "${metric}tvalue.tsf"
-#fixel2tsf "template/stats_${metric}/uncorrected_pvalue.mif" tracks_100k_sift.tck "${metric}uncorrected_pvalue.tsf"
-#fixel2tsf "template/stats_${metric}/Zstat.mif" tracks_100k_sift.tck "${metric}Zstat.tsf"
-
-# Visualise track scalar files using the tractogram tool in MRview. First load the streamlines (tracks_100k_sift.tck). Then right click and select ‘colour by (track) scalar file’. For example you might load the abs_effect_size.tsf file. Then to dynamically threshold (remove) streamline points by p-value select the “Thresholds” dropdown and select “Separate Scalar file” to load fwe_pvalue.tsf.
-
-mrview -load template/wmfod_template.mif -tractography.load template/tracks_100k_sift.tck -tractography.tsf_load "template/${metric}_fwe_1mpvalue.tsf" -tractography.tsf_thresh 0.95,1 -mode 4 -plane 2 -tractography.tsf_range 0.95,1
+# Map streamlines to the statistic and then visualize with mrview
+if [ $contrast == "sup" ]; then
+    fixel2tsf "template/stats_${metric}_${comp}/${stat}_t1.mif" template/tracks_100k_sift.tck "template/tsf_files/${metric}_${comp}_${stat}_t1.tsf" -force
+    mrview -load template/wmfod_template.mif -tractography.load template/tracks_100k_sift.tck -tractography.tsf_load "template/${metric}_${comp}_${stat}_t1.tsf" -tractography.tsf_thresh 0.95,1 -tractography.tsf_range 0.95,1 -mode 4 -plane 2 
+else
+    fixel2tsf "template/stats_${metric}_${comp}/${stat}_t2.mif" template/tracks_100k_sift.tck "template/tsf_files/${metric}_${comp}_${stat}_t2.tsf" -force
+    mrview -load template/wmfod_template.mif -tractography.load template/tracks_100k_sift.tck -tractography.tsf_load "template/${metric}_${comp}_${stat}_t2.tsf" -tractography.tsf_thresh 0.95,1 -tractography.tsf_range 0.95,1 -tractography.tsf_colourmap 2 -mode 4 -plane 2 
+fi
