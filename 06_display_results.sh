a596e0c1cb89c6305afedd87e0679e48373c1233
Ana Matoso <anamatoso92@gmail.com>
2023-05-25 18:30:54 +0100

    Correct threshold

--- 06_display_results.sh
+++ 06_display_results.sh
@@ -20,7 +20,7 @@ mkdir -p template${suffix}/tsf_files
 # Map streamlines to the statistic and then visualize with mrview
 if [ $contrast == "sup" ]; then
     fixel2tsf "template${suffix}/stats_${metric}_${comp}/${stat}_t1.mif" template${suffix}/tracks_100k_sift.tck "template${suffix}/tsf_files/${metric}_${comp}_${stat}_t1.tsf" -force
-    mrview -load template${suffix}/wmfod_template.mif -tractography.load template${suffix}/tracks_100k_sift.tck -tractography.tsf_load "template${suffix}/tsf_files/${metric}_${comp}_${stat}_t1.tsf" -mode 4 -plane 2 -fullscreen 
+    mrview -load template${suffix}/wmfod_template.mif -tractography.load template${suffix}/tracks_100k_sift.tck -tractography.tsf_load "template${suffix}/tsf_files/${metric}_${comp}_${stat}_t1.tsf" -tractography.tsf_thresh 0.95,1 -tractography.tsf_range 0.95,1 -mode 4 -plane 2 -fullscreen 
 else
     fixel2tsf "template${suffix}/stats_${metric}_${comp}/${stat}_t2.mif" template${suffix}/tracks_100k_sift.tck "template${suffix}/tsf_files/${metric}_${comp}_${stat}_t2.tsf" -force
     mrview -load template${suffix}/wmfod_template.mif -tractography.load template${suffix}/tracks_100k_sift.tck -tractography.tsf_load "template${suffix}/tsf_files/${metric}_${comp}_${stat}_t2.tsf" -tractography.tsf_thresh 0.95,1 -tractography.tsf_range 0.95,1 -tractography.tsf_colourmap 2 -mode 4 -plane 2 -fullscreen 
