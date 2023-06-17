1c08494e8ae1b289e21377167d38d7ac029f3b63
Ana Matoso <anamatoso92@gmail.com>
2023-06-17 19:34:09 +0100

    Add verification if file exists

--- scripts/06_display_results.sh
+++ scripts/06_display_results.sh
@@ -4,7 +4,7 @@ set -e
 # Set variables for vizualization
 read -p 'Choose metric [fd | log_fc | fdc]: ' metric
 read -p 'Choose statistic [fwe_1mpvalue | uncorrected_pvalue | tvalue | Zstat]: ' stat
-read -p 'Choose comparison [midinter| midpre | preict | interpre]: ' comp
+read -p 'Choose comparison [midinter| midpre | premict | interpre]: ' comp
 read -p 'Choose contrast [sup | inf]: ' contrast
 read -p 'Choose Suffix of template (use underscore if needed): ' suffix
 read -p 'Choose Atlas (use underscore if needed): ' atlas
@@ -22,10 +22,11 @@ mkdir -p template${suffix}/tsf_files
 
 # Map streamlines to the statistic and then visualize with mrview
 if [ $contrast == "sup" ]; then
-    fixel2tsf "template${suffix}/stats_results/${comp}/${metric}${atlas}/${stat}_t1.mif" \
-    template${suffix}/tracks_100k_sift.tck \
-    "template${suffix}/tsf_files/${comp}_${metric}_${stat}${atlas}_t1.tsf" -force
-    
+    if [ ! -f "template${suffix}/tsf_files/${comp}_${metric}_${stat}${atlas}_t1.tsf" ]; then # If tsf file was not created yet
+        fixel2tsf "template${suffix}/stats_results/${comp}/${metric}${atlas}/${stat}_t1.mif" \
+        template${suffix}/tracks_100k_sift.tck \
+        "template${suffix}/tsf_files/${comp}_${metric}_${stat}${atlas}_t1.tsf" -force
+    fi
     mrview -load template${suffix}/wmfod_template.mif \
     -tractography.load template${suffix}/tracks_100k_sift.tck \
     -tractography.tsf_load "template${suffix}/tsf_files/${comp}_${metric}_${stat}${atlas}_t1.tsf" \
@@ -33,10 +34,11 @@ if [ $contrast == "sup" ]; then
     -tractography.tsf_range ${limit1},${limit2} \
     -mode 4 -plane 2 -fullscreen 
 else
-    fixel2tsf "template${suffix}/stats_results/${comp}/${metric}${atlas}/${stat}_t2.mif" \
-    template${suffix}/tracks_100k_sift.tck \
-    "template${suffix}/tsf_files/${comp}_${metric}_${stat}${atlas}_t2.tsf" -force
-
+    if [ ! -f "template${suffix}/tsf_files/${comp}_${metric}_${stat}${atlas}_t2.tsf" ]; then # If tsf file was not created yet
+        fixel2tsf "template${suffix}/stats_results/${comp}/${metric}${atlas}/${stat}_t2.mif" \
+        template${suffix}/tracks_100k_sift.tck \
+        "template${suffix}/tsf_files/${comp}_${metric}_${stat}${atlas}_t2.tsf" -force
+    fi
     mrview -load template${suffix}/wmfod_template.mif \
     -tractography.load template${suffix}/tracks_100k_sift.tck \
     -tractography.tsf_load "template${suffix}/tsf_files/${comp}_${metric}_${stat}${atlas}_t2.tsf" \
