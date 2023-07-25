6ef60697a14eb2b8aba5b77f9f423529f5056b80
Ana Matoso <anamatoso92@gmail.com>
2023-07-25 16:02:21 +0100

    Update documentation

--- shell scripts/05_statistical_analysis.sh
+++ shell scripts/05_statistical_analysis.sh
@@ -20,11 +20,11 @@ atlases="JHUlabels JHUtracts AAL116"
 mkdir -p template${suffix}/stats_results
 mkdir -p template${suffix}/text_files
 
-for i in $metrics; do
+for metric in $metrics; do
     for c in $comparisons; do
         echo " "
-        echo "Starting comparison ${c} of metric ${i}"
-        python ./create_files_stats.py $c $i $suffix
+        echo "Starting comparison ${c} of metric ${metric}"
+        python ./create_files_stats.py $c $metric $suffix
 
         if [ ${c} == "midinter" ] || [ ${c} == "premict" ] || [ ${c} == "prempost" ] || [ ${c} == "prempre" ]; then 
             contrast="contrast_matrix_unpaired"
@@ -32,20 +32,41 @@ for i in $metrics; do
             contrast="contrast_matrix_paired"
         fi
         mkdir -p template${suffix}/stats_results/${c}
-        fixelcfestats template${suffix}/${i}_smooth/ \
-                        template${suffix}/text_files/files_${i}_${c}.txt \
+
+        # Perform the statistical analysis
+        fixelcfestats template${suffix}/${metric}_smooth/ \
+                        template${suffix}/text_files/files_${metric}_${c}.txt \
                         template${suffix}/text_files/design_matrix_${c}.txt \
                         template${suffix}/text_files/${contrast}.txt \
                         template${suffix}/matrix/ \
-                        template${suffix}/stats_results/${c}/${i}/ -force
+                        template${suffix}/stats_results/${c}/${metric}/ -force
+
+        # Map the fixels values to the tractogram
+        fixel2tsf "template${suffix}/stats_results/${c}/${metric}/${stat}_t1.mif" \
+				template${suffix}/tracks_100k_sift.tck \
+				"template${suffix}/tsf_files/${c}_${metric}_${stat}_t1.tsf" -force
+        fixel2tsf "template${suffix}/stats_results/${c}/${metric}/${stat}_t2.mif" \
+				template${suffix}/tracks_100k_sift.tck \
+				"template${suffix}/tsf_files/${c}_${metric}_${stat}_t2.tsf" -force
+
         for atlas in $atlases; do
-            fixelcfestats template${suffix}/${i}_smooth/ \
-                            template${suffix}/text_files/files_${i}_${c}.txt \
+            # Perform the statistical analysis
+            fixelcfestats template${suffix}/${metric}_smooth/ \
+                            template${suffix}/text_files/files_${metric}_${c}.txt \
                             template${suffix}/text_files/design_matrix_${c}.txt \
                             template${suffix}/text_files/${contrast}.txt \
                             template${suffix}/matrix/ \
-                            template${suffix}/stats_results/${c}/${i}_${atlas}/ \
+                            template${suffix}/stats_results/${c}/${metric}_${atlas}/ \
                             -mask template${suffix}/fixel_${atlas}/${atlas}_fixel.mif -force
+            
+            # Map the fixels values to the tractogram
+            fixel2tsf "template${suffix}/stats_results/${c}/${metric}${atlas}/${stat}_t1.mif" \
+				template${suffix}/tracks_100k_sift.tck \
+				"template${suffix}/tsf_files/${c}_${metric}_${stat}${atlas}_t1.tsf" -force
+			fixel2tsf "template${suffix}/stats_results/${c}/${metric}${atlas}/${stat}_t2.mif" \
+				template${suffix}/tracks_100k_sift.tck \
+				"template${suffix}/tsf_files/${c}_${metric}_${stat}${atlas}_t2.tsf" -force
         done
     done
+    /strombolihome/amatoso/end_message.sh "acabou metrica ${metric}"
 done
